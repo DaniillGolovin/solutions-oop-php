5de9021136f8645bfe2b64afea3c1f7373a72911
DaniillGolovin <daniilgolovin656@gmail.com>
2024-06-14 17:53:46 +0300

    add DeckOfCards

---
+++ src/intro-oop/DeckOfCards.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+class DeckOfCards
+{
+    public array $deck = [];
+
+    public function __construct(array $cards)
+    {
+        $this->deck = $cards;
+    }
+
+    public function getShuffled()
+    {
+        $collection = collect($this->deck);
+        $complectCards = $collection->flatMap(function ($value) {
+            return array_fill(0, 4, $value);
+        });
+        $result = $complectCards->all();
+        shuffle($result);
+        return $result;
+    }
+}
