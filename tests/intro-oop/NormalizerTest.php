33ce32cba4152b5ff4b6c2bc01a44e4dbb4f6ad4
DaniillGolovin <daniilgolovin656@gmail.com>
2024-06-16 19:43:55 +0300

    complete test

--- tests/intro-oop/NormalizerTest.php
+++ tests/intro-oop/NormalizerTest.php
@@ -2,10 +2,46 @@
 
 namespace App\Tests;
 
-use App\Normalizer;
 use PHPUnit\Framework\TestCase;
+
+use function App\Normalizer\normalize;
 
 class NormalizerTest extends TestCase
 {
+    public function testNormalize()
+    {
+        $raw = [
+            [
+                'name' => 'istambul',
+                'country' => 'turkey'
+            ],
+            [
+                'name' => 'Moscow ',
+                'country' => ' Russia'
+            ],
+            [
+                'name' => 'iStambul',
+                'country' => 'tUrkey'
+            ],
+            [
+                'name' => 'antalia',
+                'country' => 'turkeY '
+            ],
+            [
+                'name' => 'samarA',
+                'country' => '  ruSsiA'
+            ],
+        ];
 
+        $actual = normalize($raw);
+        $expected = [
+            'russia' => [
+                'moscow', 'samara'
+            ],
+            'turkey' => [
+                'antalia', 'istambul'
+            ]
+        ];
+        $this->assertEquals($expected, $actual);
+    }
 }
