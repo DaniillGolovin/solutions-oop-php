21815eeb4bdadf20d95502e189e61786335cd3de
DaniillGolovin <daniilgolovin656@gmail.com>
2024-07-01 15:04:42 +0300

    added documentation of solutions

--- src/polymorphism/HTML.php
+++ src/polymorphism/HTML.php
@@ -2,9 +2,17 @@
 
 namespace Polymorphism\HTML;
 
-//use Tightenco\Collect\Support\Collection;
 use Illuminate\Support\Collection;
 
+/**
+ * Solution Polymorphism -> 2
+ *
+ *The function retrieves links from an array of tags by key
+ *
+ * @param   array  $tags  The array of tags to process.
+ *
+ * @return array|null An array of extracted attribute values.
+ */
 function getLinks(array $tags): ?array
 {
     $mapping = [
@@ -23,13 +31,34 @@ function getLinks(array $tags): ?array
     return array_values($mappedArray);
 }
 
+/**
+ * Solution Polymorphism -> 3
+ *
+ * This function removes specific keys ('name', 'tagType', 'body') from the input array
+ * and then maps the remaining key-value pairs to a string of HTML attributes.
+ *
+ * @param   array  $tag  The associative array of HTML tag attributes.
+ *
+ * @return string The string of HTML attributes.
+ */
 function buildAtts(array $tag)
 {
     return collect($tag)->except('name', 'tagType', 'body')
         ->map(fn($value, $key) => " {$key}=\"{$value}\"")->join('');
 }
 
-function stringify(array $tag)
+/**
+ * Solution Polymorphism 3
+ *
+ * This function determines if the tag is a 'single' or 'pair' tag and constructs
+ * the appropriate HTML string. 'Single' tags are self-closing, while 'pair' tags
+ * have an opening and closing tag with content in between.
+ *
+ * @param array  $tag  The associative array of HTML tag attributes.
+ *
+ * @return array|null The string of HTML attributes.
+ */
+function stringify(array $tag): ?array
 {
     $attrs = buildAtts($tag);
 
--- src/polymorphism/LinkedList.php
+++ src/polymorphism/LinkedList.php
@@ -4,6 +4,15 @@ namespace Polymorphism\LinkedList;
 
 use Polymorphism\Node;
 
+/**
+ * Solution Polymorphism -> 1
+ *
+ * This function takes the head node of a singly linked list and reverses the order
+ * of nodes, returning the new head node of the reversed list.
+ *
+ * @param Node $list The head node of the singly linked list to be reversed.
+ * @return Node The head node of the reversed singly linked list.
+ */
 function reverse(Node $list): Node
 {
     $reversedList = null;
